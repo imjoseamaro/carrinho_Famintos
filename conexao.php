799c8fca79adf621e59f1ce0ffbb8bc8926eec26
imjoseamaro <joseamaro.lemosneto@gmail.com>
2023-02-22 12:16:01 -0300

    Revert "Carrinho de Compras"
    This reverts commit 4606bacd2780140f165a5421ee32f84edd52d8a6.

--- conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servidor = "localhost"; //servidor local
-    $usuario = "root";   //usuario root (qualquer)
-    $senha = ""; //senha nula
-    $banco = "db_famintos"; //nome do banco de dados
-
-    $cn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha); //conexão
